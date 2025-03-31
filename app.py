3fd9e3725d3f90470a31f1c7cbfbfd32b8d1366f
RAFAPFS <rafael.posadas@pfsgroup.es>
2025-03-30 20:20:58 -0600

    #

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="Mi App", page_icon="📚", layout="wide")
+st.set_page_config(page_title="AAAA", page_icon="📚", layout="wide")
 
-st.title("Bienvenido a Mi Aplicación")
+st.title("Mis Ejercicios de Streamlit")
+st.write("Rafael Posadas Ruíz")
 st.write("Selecciona una opción en el menú de la izquierda para continuar.")
 
 # Streamlit muestra automáticamente las páginas dentro de "pages/"
-st.sidebar.success("Selecciona una opción en el menú.")
+#st.sidebar.success("Selecciona una opción en el menú.")
